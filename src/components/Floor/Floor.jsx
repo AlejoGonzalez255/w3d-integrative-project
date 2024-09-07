0c600c59023a9ace34b1e98b3d72a529ae66f279
MiguelPineda <pineda.miguel@correounivalle.edu.co>
2024-09-06 20:59:22 -0500

    Update Floor.jsx

--- src/components/Floor/Floor.jsx
+++ src/components/Floor/Floor.jsx
@@ -6,7 +6,7 @@ const Floor = () => {
   return (
     <mesh ref={floorRef} rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
       <planeGeometry args={[100, 100]} />
-      <meshStandardMaterial color="lightgray" />
+      <meshStandardMaterial color="#19b831" />
     </mesh>
   );
 };

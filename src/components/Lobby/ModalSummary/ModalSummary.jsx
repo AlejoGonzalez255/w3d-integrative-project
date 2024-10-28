f886e774479d91aba4b35d22b718d33ec0b13c3d
AlejoGonzalez255 <162381038+AlejoGonzalez255@users.noreply.github.com>
2024-10-28 12:41:08 -0500

    fix ModalSummary

--- src/components/Lobby/ModalSummary/ModalSummary.jsx
+++ src/components/Lobby/ModalSummary/ModalSummary.jsx
@@ -3,10 +3,8 @@ import Modal from 'react-bootstrap/Modal';
 import useModalSummaryStore from '../../../stores/use-modalSummary-state';
 import { useNavigate } from 'react-router-dom';
 
-export default function ModalSummary({show, onHide}) {
-
+export default function ModalSummary({ show, onHide }) {
   const { modalSummaryData } = useModalSummaryStore();
-
   const navigate = useNavigate();
 
   return (
@@ -17,19 +15,32 @@ export default function ModalSummary({show, onHide}) {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          {modalSummaryData.title}
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>
-          {modalSummaryData.description}
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={() => navigate(modalSummaryData.navigate)}>Close</Button>
-      </Modal.Footer>
+      <div style={{ border: '5px solid green', borderRadius: '8px', padding: '10px' }}>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {modalSummaryData.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            {modalSummaryData.description}
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button 
+            onClick={() => navigate(modalSummaryData.navigate)} 
+            style={{ backgroundColor: '#4CAF50', color: 'white', border: 'none' }}
+          >
+            Ir al lugar
+          </Button>
+          <Button 
+            onClick={onHide} 
+            style={{ backgroundColor: '#f44336', color: 'white', border: 'none' }}
+          >
+            Cerrar
+          </Button>
+        </Modal.Footer>
+      </div>
     </Modal>
   );
 }

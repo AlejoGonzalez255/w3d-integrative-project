277de93755f49c163e2ad087ecbd1d6e2cebf5f4
AlejoGonzalez255 <162381038+AlejoGonzalez255@users.noreply.github.com>
2024-10-28 13:14:39 -0500

    add shadows

--- src/components/lights/Lights.jsx
+++ src/components/lights/Lights.jsx
@@ -2,22 +2,24 @@ const Lights = () => {
   return (
     <>
       <ambientLight intensity={1.5} />
-      <directionalLight 
-      color={"#ffffff"}
-      position={[100, 100, 100]}
-      intensity={3} 
-      castShadow
-      shadow-camera-left={5}
-      shadow-camera-right={5}
-      shadow-camera-top={5}
-      shadow-camera-bottom={5}
-      shadow-camera-far={5}
-      shadow-mapSize-width={1}
-      shadow-mapSize-height={1}
-      shadow-normalBias={0.3}
+      <directionalLight
+        color={"#ffffff"}
+        position={[100, 100, 100]}
+        intensity={3}
+        castShadow
+        shadow-camera-left={-50}
+        shadow-camera-right={50}
+        shadow-camera-top={50}
+        shadow-camera-bottom={-50}
+        shadow-camera-far={200}
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-normalBias={0.05}
       />
+
     </>
   );
 };
 
 export default Lights;
+

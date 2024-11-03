15bcdcd01ce315e8e5c0c547afe673db436742c0
EstebanUniValle <esteban.charria@correounivalle.edu.co>
2024-11-03 14:58:42 -0500

    styles modal

--- src/components/Lobby/ModalSummary/ModalSummary.jsx
+++ src/components/Lobby/ModalSummary/ModalSummary.jsx
@@ -2,6 +2,8 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import useModalSummaryStore from '../../../stores/use-modalSummary-state';
 import { useNavigate } from 'react-router-dom';
+import "./ModalSummary.css";
+import ButtonPlant from '../../ButtonPlant/ButtonPlant';
 
 export default function ModalSummary({ show, onHide }) {
   const { modalSummaryData } = useModalSummaryStore();
@@ -15,7 +17,6 @@ export default function ModalSummary({ show, onHide }) {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <div style={{ border: '5px solid green', borderRadius: '8px', padding: '10px' }}>
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
             {modalSummaryData.title}
@@ -28,19 +29,18 @@ export default function ModalSummary({ show, onHide }) {
         </Modal.Body>
         <Modal.Footer>
           <Button 
-            onClick={() => navigate(modalSummaryData.navigate)} 
-            style={{ backgroundColor: '#4CAF50', color: 'white', border: 'none' }}
-          >
-            Ir al lugar
-          </Button>
-          <Button 
             onClick={onHide} 
             style={{ backgroundColor: '#f44336', color: 'white', border: 'none' }}
           >
             Cerrar
           </Button>
+          <Button 
+            onClick={() => navigate(modalSummaryData.navigate)} 
+            style={{ backgroundColor: '#4CAF50', color: 'white', border: 'none' }}
+          >
+            Ir al lugar
+          </Button>
         </Modal.Footer>
-      </div>
     </Modal>
   );
 }

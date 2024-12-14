a539bdb9fa5f8dffc9a1452160545c3988445754
MiguelPineda <pineda.miguel@correounivalle.edu.co>
2024-12-13 19:35:43 -0500

    Update Vegetation.jsx

--- src/components/ErosionSuelo/Vegetation/Vegetation.jsx
+++ src/components/ErosionSuelo/Vegetation/Vegetation.jsx
@@ -7,7 +7,7 @@ export function Vegetation(props) {
 
   const arbolSecoPosiciones = [
     [-48, 0, -48], [-42, 0, -46], [-44, 0, -38], [-40, 0, -44], [-35, 0, -40],
-    [-30, 0, -48], [-28, 0, -42], [-20, 0, -44], [-15, 0, -38], [-10, 0, -46],
+    [-30, 0, -48], [-28, 0, -42], [-20, 0, -44], [-15, 0, -38], [-10, 0, -46], 
     [-5, 0, -40], [8, 0, -45], [15, 0, -42], [20, 0, -38], [25, 0, -48],
     [30, 0, -44], [35, 0, -42], [40, 0, -38], [45, 0, -48], [-48, 0, -30],
     [-42, 0, -28], [-38, 0, -25], [-30, 0, -35], [-28, 0, -20], [-15, 0, -30],
@@ -15,14 +15,14 @@ export function Vegetation(props) {
     [25, 0, -20], [30, 0, -25], [40, 0, -30], [48, 0, -35], [-48, 0, -10],
     [-42, 0, -5], [-38, 0, 5], [-30, 0, -10], [-25, 0, 5], [-20, 0, -5],
     [-5, 0, -5], [5, 0, -5], [10, 0, -15], [15, 0, -10], [20, 0, -5],
-    [25, 0, 0], [30, 0, -15], [38, 0, -10], [42, 0, -5], [48, 0, 5],
+    [25, 0, 0], [30, 0, -15], [38, 0, -10], [42, 0, -5], [48, 0, 5], 
     [48, 0, 15], [-25, 0, 35], [-18, 0, 38], [-12, 0, 20], [5, 0, -40],
-    [20, 0, -35], [32, 0, -30], [43, 0, -25], [47, 0, -20], [40, 0, -10]
+    [20, 0, -35], [32, 0, -30], [43, 0, -25], [47, 0, -20], [40, 0, -10],
   ];
-  
+
   const arbolSeco2Posiciones = [
     [-15, 0, 10], [-10, 0, -15], [-5, 0, 10], [8, 0, -10], [15, 0, 15],
-    [25, 0, -10], [30, 0, 5], [38, 0, 15], [42, 0, -8], [45, 0, -5], [48, 0, 0],
+    [25, 0, -10], [30, 0, 5], [38, 0, 15], [42, 0, -8], [45, 0, -5],
     [-48, 0, 10], [-44, 0, 15], [-42, 0, 20], [-35, 0, 10], [-28, 0, 15],
     [-25, 0, 20], [-20, 0, 25], [-15, 0, 30], [-10, 0, 35], [-5, 0, 40],
     [8, 0, 30], [15, 0, 38], [20, 0, 48], [25, 0, 44], [30, 0, 42],
@@ -31,19 +31,23 @@ export function Vegetation(props) {
     [-20, 0, 40], [-15, 0, 38], [-10, 0, 44], [-5, 0, 48], [8, 0, 42],
     [15, 0, 45], [20, 0, 38], [25, 0, 44], [30, 0, 48], [35, 0, 42],
     [40, 0, 38], [45, 0, 48], [-48, 0, -15], [-44, 0, -5], [-38, 0, 0],
-    [-30, 0, -15], [-28, 0, -10], [-20, 0, -5], [-15, 0, 0], [-10, 0, -15]
+    [-30, 0, -15], [-28, 0, -10], [-20, 0, -5], [-15, 0, 0], [-10, 0, -15],
   ];
-  
-  const rocaPosiciones = [];
-  const troncoPosiciones = [];
-  const troncoCaidoPosiciones = [];
+
+  const rocaPosiciones = [
+    [31, 0, -40], [40, 0, -48], [-31, 0, 14], [-20, 0, 10], [-16, 0, 25],
+    [21, 0, 7], [12, 0, 5], [18, 0, -15], [6, 0, 1], [-14, 0, 40], 
+    [35, 0, 19], [17, 0, 21], [4, 0, 29], [-31, 0, 40], [35, 0, 19], 
+    [-30, 0, 21], [-5, 0, -14], [-3, 0, 20], [-15, 0, 19], [-8, 0, -10]
+  ];
+  const troncoPosiciones = [[19, 0, 18], [-5, -0.4, 16], [25, -0.4, 11]];
+  const troncoCaidoPosiciones = [[-5, -0.4, 16], [-15, -0.4, -20], [5, -0.4, -8]];
 
   return (
     <group {...props} dispose={null}>
       {rocaPosiciones.map((posicion, index) => (
-        <RigidBody type="fixed" colliders={false}>
+        <RigidBody type="fixed" colliders="trimesh" key={`roca-${index}`}>
           <mesh
-            key={`roca-${index}`}
             position={posicion}
             scale={4}
             castShadow
@@ -51,14 +55,12 @@ export function Vegetation(props) {
             geometry={nodes.Icosphere001.geometry}
             material={materials.Material}
           />
-          <CuboidCollider args={[0.6, 0.6, 0.6]} position={posicion} />
         </RigidBody>
       ))}
 
       {arbolSecoPosiciones.map((posicion, index) => (
-        <RigidBody type="fixed" colliders="trimesh">
+        <RigidBody type="fixed" colliders="trimesh" key={`arbol-seco-${index}`}>
           <mesh
-            key={`arbol-seco-${index}`}
             castShadow
             receiveShadow
             geometry={nodes.Plane001.geometry}
@@ -70,9 +72,12 @@ export function Vegetation(props) {
       ))}
 
       {arbolSeco2Posiciones.map((posicion, index) => (
-        <RigidBody type="fixed" colliders="trimesh">
+        <RigidBody
+          type="fixed"
+          colliders="trimesh"
+          key={`arbol-seco2-${index}`}
+        >
           <mesh
-            key={`arbol-seco2-${index}`}
             castShadow
             receiveShadow
             geometry={nodes.Plane003.geometry}
@@ -82,9 +87,10 @@ export function Vegetation(props) {
           />
         </RigidBody>
       ))}
+
       {troncoPosiciones.map((posicion, index) => (
-        <RigidBody type="fixed" colliders={false}>
-          <group key={`tronco-${index}`} position={posicion} scale={0.8}>
+        <RigidBody type="fixed" colliders="trimesh" key={`tronco-${index}`}>
+          <group position={posicion} scale={0.8}>
             <mesh
               castShadow
               receiveShadow
@@ -104,12 +110,16 @@ export function Vegetation(props) {
               material={materials["tree 3"]}
             />
           </group>
-          <CuboidCollider args={[0.7, 1, 0.7]} position={posicion} />
         </RigidBody>
       ))}
+
       {troncoCaidoPosiciones.map((posicion, index) => (
-        <RigidBody type="fixed" colliders="cuboid">
-          <group key={`tronco-caido-${index}`} position={posicion} scale={0.8}>
+        <RigidBody
+          type="fixed"
+          colliders="trimesh"
+          key={`tronco-caido-${index}`}
+        >
+          <group position={posicion} scale={0.8}>
             <mesh
               castShadow
               receiveShadow
